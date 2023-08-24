c83bae043ac1e24cafac0261820962f842221794
marks <75466900@qq.com>
2023-08-24 16:22:29 +0800

    ✨ feat: 新增 k v 存储

--- lib/global.dart
+++ lib/global.dart
@@ -5,11 +5,14 @@ import 'common/index.dart';
 
 class Global {
   static Future<void> init() async {
+    /// 初始化
     WidgetsFlutterBinding.ensureInitialized();
 
     await Future.wait([
+      /// 工具类
+      Storage().init(),
+      /// 服务类
       Get.putAsync<ConfigService>(() async => await ConfigService().init()),
-    ]).whenComplete(() {
-    });
+    ]).whenComplete(() {});
   }
 }
