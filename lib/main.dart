7a76fe079b8c235d373ce2499188c58740df5a3c
marks <75466900@qq.com>
2023-08-29 17:28:33 +0800

    ✨ feat: 新增屏幕尺寸适配

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
@@ -15,22 +16,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
+    return ScreenUtilInit(
+      designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+      // splitScreenMode: false, // 支持分屏尺寸
+      // minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+      // 一般返回一个MaterialApp类型的Function()
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
 
-      // 样式
-      theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
+          // 样式
+          theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
 
-      initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
 
-      // 多语言
-      translations: Translation(), // 词典
-      localizationsDelegates: Translation.localizationsDelegates, // 代理
-      supportedLocales: Translation.supportedLocales, // 支持的语言
-      locale: ConfigService.to.locale, // 当前语言
-      fallbackLocale: Translation.fallbackLocale, // 默认语言
+          // 多语言
+          translations: Translation(), // 词典
+          localizationsDelegates: Translation.localizationsDelegates, // 代理
+          supportedLocales: Translation.supportedLocales, // 支持的语言
+          locale: ConfigService.to.locale, // 当前语言
+          fallbackLocale: Translation.fallbackLocale, // 默认语言
+
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+              child: widget!,
+            );
+          },
+
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
