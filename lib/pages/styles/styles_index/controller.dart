c86a5264b437ed170c915246911ccc03bad56b68
marks <75466900@qq.com>
2023-08-30 17:48:01 +0800

    ✨ feat: 新增text_form组件

--- lib/pages/styles/styles_index/controller.dart
+++ lib/pages/styles/styles_index/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_woo_commerce/common/index.dart';
 import 'package:get/get.dart';
 
@@ -30,6 +31,7 @@ class StylesIndexController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    FlutterNativeSplash.remove();
     _initData();
   }
 }
