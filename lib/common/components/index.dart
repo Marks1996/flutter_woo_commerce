534b8d8f5975765ccd1d6ae39f7344af1507db1b
marks <75466900@qq.com>
2023-09-01 10:32:14 +0800

    ✨ feat: 新增商品列表

--- lib/common/components/index.dart
+++ lib/common/components/index.dart
@@ -9,3 +9,4 @@ export 'category_item.dart';
 export 'product_item.dart';
 export 'refresher.dart';
 export 'placehold.dart';
+export 'appbar.dart';
--- lib/pages/goods/home/controller.dart
+++ lib/pages/goods/home/controller.dart
@@ -105,7 +105,14 @@ class HomeController extends GetxController {
   void onCategoryTap(int categoryId) {}
 
   // ALL 点击事件
-  void onAllTap(bool featured) {}
+  void onAllTap(bool featured) {
+    Get.toNamed(
+      RouteNames.goodsProductList,
+      arguments: {
+        "featured": featured,
+      },
+    );
+  }
 
   // 导航点击事件
   void onAppBarTap() {}
